26f4b1545e9671de3f0333e156470a93a6da8f17
emilyjryan <emilyryanmac@gmail.com>
2022-12-06 12:33:23 -0800

    finished all phrase classes, updated correct screen to display language answer

--- DOM-language-quiz-game/js/DOM-script.js
+++ DOM-language-quiz-game/js/DOM-script.js
@@ -22,7 +22,7 @@ const playBoxes = document.querySelector('#play-boxes')
 const levelBox = document.querySelector('#level-box')
 const phraseText = document.querySelector('#phrase-text')
 const translationText = document.querySelector('#translation-text')
-const factText = document.querySelector('#fact-text')
+const hintText = document.querySelector('#hint-text')
 const resultText = document.querySelector('#result-text')
 
 // ===== DEFINE CLASSES ===== //
@@ -34,12 +34,13 @@ class Phrase {
     static score = 0
 
     // Constructor:
-    constructor (level, phrase, language, translation, fact) {
-        this.level = `Level ${level}:`
+    constructor (level, phrase, language, translation, hint, flag) {
+        this.level = `LEVEL ${level}:`
         this.phrase = phrase
         this.language = language
         this.translation = `This means ${translation} in what language?`
-        this.fact = `(Fun fact: ${fact})`
+        this.hint = `(Hint: ${hint})`
+        this.flag = flag
     }
 
     // Methods:
@@ -48,7 +49,7 @@ class Phrase {
         levelBox.innerText = this.level
         phraseText.innerText = this.phrase
         translationText.innerText = this.translation
-        factText.innerText = this.fact
+        hintText.innerText = this.hint
         
 
     }
@@ -56,13 +57,13 @@ class Phrase {
     checkGuess () {
         resultText.style.display = 'block'
         if (inputBox.value.toLowerCase() === this.language) {
-        resultText.innerText = "Correct! Nice work! 😎"
+        resultText.innerText = `Correct! The language is ${this.language} ${this.flag}`
         Phrase.score++
         console.log(Phrase.score)
         } else if (inputBox.value === '') {
         resultText.innerText = "Take a guess!" 
         } else {
-            resultText.innerText = "Sorry, that's incorrect. Press 'Next' to move on!"
+            resultText.innerText = `Sorry, that's incorrect. The language is ${this.language} ${this.flag}. Press 'Next' to move on!`
             console.log(Phrase.score)
         }
     }
@@ -71,19 +72,94 @@ class Phrase {
 
 // ===== Phrases List ===== //
 
-const spanish = new Phrase ("1", "\"Mi casa es su casa.\"", "spanish", "\"my house is your house\"", "You might want to go salsa dancing with someone who speaks this language 🕺🏽💃🏻")
-const french = new Phrase ("2", "\"C'est la vie.\"", "french", "\"this is the life\"", "Think baguette, beret, croissant, a giant tower...")
-const german = new Phrase ("3", "\"Es freut mich Sie kennenzulernen\"", "german", "\"nice to meet you\"", "makes me want to wear a lederhosen while eating a snitzel, amiright?")
-const korean = new Phrase ("4", "\"감사합니다 (gamsahamnida)\"", "korean", "\"thank you\"", )
-const japanese = new Phrase ("5", "\"愛してる(aishi teru)\"", "japanese", "\"i love you\"")
-const hindi = new Phrase ("6", "\"मोसम केसा हे\? (mosam kesa he\?)\"", "hindi", "\"how is the weather?\"")
-const swedish = new Phrase ("7", "\"jag är riktigt hungrig!\"", "swedish", "\"i'm really hungry\"")
-const chinese = new Phrase ("8", "\"你好吗\?(nǐ hǎo ma?)\"", "chinese", "\"how are you\"")
-const russian = new Phrase ("9", "\"я люблю языки (ya lyublyu yazyki)\"", "russian", "\"i love languages\"")
-const icelandic = new Phrase ("10", "\"þetta er síðasta tungumálið\"", "icelandic", "\"this is the last language\"")
+const spanish = new Phrase (
+    "1", 
+    "\"Mi casa es su casa.\"", 
+    "spanish", 
+    "\"my house is your house\"", 
+    "You might want to go salsa dancing with someone who speaks this language 🕺🏽💃🏻",
+    "🇲🇽🇪🇸"
+)
 
+const french = new Phrase (
+    "2", 
+    "\"C'est la vie.\"", 
+    "french", 
+    "\"this is the life\"", 
+    "Think berets 👩🏻‍🎨, croissants 🥐, a giant tower...",
+    "🇫🇷"
+)
 
+const german = new Phrase (
+    "3", 
+    "\"Es freut mich Sie kennenzulernen\"", 
+    "german", 
+    "\"nice to meet you\"", 
+    "Makes me want to wear a lederhosen while eating a schnitzel, amiright?",
+    "🇩🇪"
+)
 
+const korean = new Phrase (
+    "4", 
+    "\"감사합니다 (gamsahamnida)\"", 
+    "korean", 
+    "\"thank you\"",
+    "All the members of your fav KPOP band BTS speak this language",
+    "🇰🇷"
+)
+
+const japanese = new Phrase (
+    "5", 
+    "\"愛してる(aishi teru)\"", 
+    "japanese", 
+    "\"i love you\"",
+    "Land of cherry blossoms 🌸 and allllll the sushi 🍣",
+    "🇯🇵"
+)
+
+const hindi = new Phrase (
+    "6", 
+    "\"मोसम केसा हे\? (mosam kesa he\?)\"", 
+    "hindi", 
+    "\"how is the weather?\"",
+    "The national symbol of this language's main country is a really big kitty 🐅",
+    "🇮🇳"
+)
+
+const swedish = new Phrase (
+    "7", 
+    "\"jag är riktigt hungrig!\"", 
+    "swedish", 
+    "\"i'm very hungry\"",
+    "Master this language and you'd be a pro at pronouncing IKEA product names",
+    "🇸🇪"
+)
+
+const chinese = new Phrase ("8", 
+    "\"你好吗\?(nǐ hǎo ma?)\"", 
+    "chinese", 
+    "\"how are you\"",
+    "Speakers of this language could walk along a super long wall with a GREAT view",
+    "🇨🇳"
+)
+
+const russian = new Phrase (
+    "9", 
+    "\"я люблю языки (ya lyublyu yazyki)\"", 
+    "russian", 
+    "\"i love languages\"",
+    "Is it tsar or czar? I always forget",
+    "🇷🇺"
+)
+
+const icelandic = new Phrase (
+    "10", 
+    "\"þetta er síðasta tungumálið\"", 
+    "icelandic", 
+    "\"this is the last language\"",
+    "Walter Mitty was truly living his best life skateboarding down those hills",
+    "🇮🇸"
+)
 
 
 const allPhrases = [spanish, french, german, korean, japanese, hindi, swedish, chinese, russian, icelandic]
@@ -100,7 +176,7 @@ const instructionFunction = function () {
     langStats.style.display = 'block'
     instructionsBox.style.display = 'block'
     main.style.backgroundImage = 'none'
-    instructionsBox.innerText = "There are over 7,000 languages spoken around the globe! Think you could identify some of them? You'll be given phrases from random languages and it's up to you to identify them to earn points. You will also be given a fun fact about each language to help you in your identification. When a phrase appears, type in your best guess and then click 'Submit'. If you guess correctly, you'll earn 1 point. Press 'Play' to start!"}
+    instructionsBox.innerText = "There are over 7,000 languages spoken around the globe! Think you could identify some of them? You'll be given phrases from random languages and it's up to you to identify them to earn points. You will also be given a hint about each language to help you in your identification. When a phrase appears, type in your best guess and then click 'Submit'. If you guess correctly, you'll earn 1 point. Press 'Play' to start!"}
 
 // Instructions button:
 instructionsBtn.addEventListener('click', () => {
@@ -177,7 +253,8 @@ restartBtn.addEventListener('click', () => {
     console.log(Phrase.score)
     playBoxes.style.display = 'none'
     resultText.style.display = 'none'
-    main.style.backgroundImage = "url('../images/languages-around-the-world-1024x710.png')";
+    main.style.backgroundImage = "url('../images/welcome-diff-langs.jpeg')";
+    inputBox.innerText = ''
     inputBox.style.display = 'none'
     submitBtn.style.display = 'none'
     nextBtn.style.display = 'none'
